e071c124e818ffff23495f8f4f2d9dcb5d31c4ad
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-11-16 19:29:53 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -17,18 +17,17 @@ if (navigator.serviceWorker) {
  * this function
  */
 function myButtonClicked() {
-  const radioButton1 = document.getElementById('option-1').checked
+  const radioButton1 = document.getElementById("option-1").checked
 
   if (radioButton1 == true) {
     const randomNumber = Math.floor(Math.random() * 6) + 1
 
     document.getElementById("value").innerHTML =
-    "you picked option positive. your number is "+ randomNumber+ "."
-  }
-  else {
-    const randomNegativeNumber = Math.floor(Math.random() * -6) + (-1)
+      "you picked option positive. your number is " + randomNumber + "."
+  } else {
+    const randomNegativeNumber = Math.floor(Math.random() * -6) + -1
 
     document.getElementById("value").innerHTML =
-    "you picked option negative. your number is "+ randomNegativeNumber+ "."
+      "you picked option negative. your number is " + randomNegativeNumber + "."
   }
 }
